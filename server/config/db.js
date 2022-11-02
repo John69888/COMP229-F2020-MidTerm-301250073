637e35854f7117d93f80471526922a586f8a91dd
John69888Canada <cliu162@my.centennialcollege.ca>
2022-11-02 12:11:09 -0500

    working v1.0

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI":"mongodb+srv://john69888:Hl99Hl99@cluster0.szdzazh.mongodb.net/book_store?retryWrites=true&w=majority",
+  "Secret": 'SomeSecret'
+
+};
